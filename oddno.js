b9ec83b9a18eabb544dd1c956595086ee163eda9
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2022-01-26 11:08:14 +0530

    Create oddno.js

---
+++ oddno.js
@@ -0,0 +1,12 @@
+for (var x=0; x<=15; x++) {
+        if (x % 2 !== 0) {
+                console.log(x + " is odd");   
+        }
+        
+}
+
+
+
+for (let i = 1; i < 100; i+=2) {
+ console.log(i);
+}
