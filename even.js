4f87108f2e8d08d143d59454ec2c4882d24b71c4
rohitbansal11 <80095887+rohitbansal11@users.noreply.github.com>
2022-01-26 11:01:46 +0530

    Create even.js

---
+++ even.js
@@ -0,0 +1,18 @@
+var counter = 0;
+var line=[];
+
+for (var i = 1; i <= 100; i++) {
+  if (i % 2 == 0){
+    if (line.length < 5) {
+      line[counter] = i;
+      counter++
+    } else {
+      console.log(line);
+      line=[];
+      counter = 0;
+      line[counter] = i;
+      counter++
+    }
+  }
+}
+console.log(line);
